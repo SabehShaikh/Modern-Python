7ce154feba61f61d577d761597f45351138613cf
SabehShaikh <sabehshaikh201@gmail.com>
2024-04-12 11:47:05 +0500

    class 01 code updated.

--- 01_class(Helloworld_String)/class1.py
+++ 01_class(Helloworld_String)/class1.py
@@ -1,1 +1,17 @@
-print("Pakistan")
+# name : list[str] = ['a' , 'b' , 'c']
+# print(name) # print
+# print(type(name)) # type
+# print(id(name)) #physical address
+# print([i for i in dir(name) if "_" not in i]) #methods and attributes
+
+# name : tuple[str , int, float] = ('pakistan' , 7 , 2.0)
+# print(name) # print
+# print(type(name)) # type
+# print(id(name)) #physical address
+# print([i for i in dir(name) if "_" not in i]) #methods and attributes
+
+# name : any = "Pakistan"
+# print(name) # print
+# print(type(name)) # type
+# print(id(name)) # physcial address
+# print([i for i in dir(name) if "__" not in i]) # methods and attributes
